2b504a834e731ac9bdbb305095b11778ea7c6ea0
Resnov25 <andre_viggar@hotmail.com>
2022-03-23 10:21:06 -0600

    Actualizacion general

--- src/main/java/com/andre/api/controller/PolizaController.java
+++ src/main/java/com/andre/api/controller/PolizaController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,4 +31,5 @@ public class PolizaController {
         }
         return new ResponseEntity<>(null, HttpStatus.CONFLICT);
     }
+
 }
---
+++ src/main/java/com/andre/api/repository/PolizaRepositoryImpl.java
@@ -0,0 +1,17 @@
+package com.andre.api.repository;
+
+import com.andre.api.po.Poliza;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public class PolizaRepositoryImpl {
+
+    public Optional<Poliza> findById(Integer n) {
+        return Optional.empty();
+    }
+
+    public Poliza save(Poliza p) {
+        return null;
+    }
+}
